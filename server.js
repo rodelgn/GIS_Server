102570609c8ad3f2b9d9b6506a9a1f35e2a663b3
dinesdayrit <dayritdines22@gmail.com>
2023-10-25 23:41:14 +0800

    change port for testing

--- server.js
+++ server.js
@@ -5,4 +5,6 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
