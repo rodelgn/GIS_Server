e1c87951b6f621b0a5e4d0ce018c9d5425cddf06
dinesdayrit <dayritdines22@gmail.com>
2023-10-26 00:04:13 +0800

    update port

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-server.listen(port, '0.0.0.0',() => {
+server.listen(port, '129.150.47.67',() => {
     console.log(`Server is running on port ${port}`);
 });
